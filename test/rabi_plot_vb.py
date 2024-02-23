e6b25add1111e9b67fd76b6a1405e0b374440cef
vebjorhb <vebjorn.bakkestuen@oslomet.no>
2024-02-23 11:12:32 +0100

    updated plotting to new code and added functional constraint

--- test/rabi_plot_vb.py
+++ test/rabi_plot_vb.py
@@ -9,18 +9,17 @@ from qmodel import oscillator, spin, func
 import seaborn as sns
 
 plt.style.use('seaborn-v0_8')
-sns.set(font_scale=1.3)
 plt.rc('text', usetex=True)                                                     # Setting LaTex fonts and style
-plt.rc('font', family='serif', size=16)                                         # as well as the desired text size
-plt.rcParams['axes.facecolor'] = 'none'
+plt.rc('font', family='serif', size=40)                                         # as well as the desired text size
 
-colors = sns.color_palette('rocket', as_cmap=True)
-sns.set(font_scale=1.5)
+sns.set(font_scale=2)
+plt.rcParams['figure.facecolor'] = 'none'
+plt.rc('lines', linewidth=2,markersize=10)
 
 t = 1
 lam = 1
 j = 0
-v = 0.2
+v = 0.02
 oscillator_size = 10
 
 ## Rabi model in 2nd quantization basis
@@ -31,36 +30,77 @@ B = OB.tensor(SB)
 XOperator = ((oscillator.Creator(OB) + oscillator.Annihilator(OB)) / sqrt(2)).extend(B)
 SigmaX = spin.SigmaX(SB).extend(B)
 SigmaZ = spin.SigmaZ(SB).extend(B)
+
 CouplingRabi = SigmaZ*XOperator
-CouplingJC = spin.SigmaMinus(SB).extend(B)*oscillator.Creator(OB).extend(B) + spin.SigmaPlus(SB).extend(B)*oscillator.Annihilator(OB).extend(B)
-H0_KS = -t*SigmaX + oscillator.Hamiltonian(OB).extend(B) # Hamiltonian, just kinetic
+H0_KS = -t*SigmaX + oscillator.Kinetic(OB).extend(B) # Hamiltonian, just kinetic
 H0_full = H0_KS + lam*CouplingRabi # Hamiltonian, with coupling
 H = H0_full + j*XOperator + v*SigmaZ # Hamiltonian, with external potential and current
 
 def plot_functionals():
     sigma_space = np.linspace(-1,1,201)
-    
-    E_full = func.EnergyFunctional(H0_full, SigmaZ)
-    E_KS = func.EnergyFunctional(H0_KS, SigmaZ)
+#    
+#    E_full = func.EnergyFunctional(H0_full, SigmaZ)
+#    E_KS = func.EnergyFunctional(H0_KS, SigmaZ)
+#    F_full = []
+#    F_full_eps = []
+#    F_KS = []
+#    E_c = []
+#    eps = 0.1
+#    
+#    for sigma in sigma_space:
+#        F_full.append(E_full.legendre_transform(sigma))
+#        F_full_eps.append(E_full.legendre_transform(sigma, epsMY=eps))
+#        F_KS.append(E_KS.legendre_transform(sigma))
+#        E_c.append(F_full[-1] - F_KS[-1])
+    lam = 1
+    x = 0
+    eps = 0.1
+
+    E_full = func.EnergyFunctional(H0_KS + lam*CouplingRabi, [SigmaZ, XOperator])
+    E_KS = func.EnergyFunctional(H0_KS, [SigmaZ, XOperator])
     F_full = []
     F_full_eps = []
     F_KS = []
-    E_c = []
-    eps = 0.1
+    E_xc = []
+    test_F_full = []
+    test_F_KS = []
+    W_0 = []
+    W_c = []
+    T_c = []
+    test_E_xc = []
     
     for sigma in sigma_space:
-        F_full.append(E_full.legendre_transform(sigma))
-        F_full_eps.append(E_full.legendre_transform(sigma, epsMY=eps))
-        F_KS.append(E_KS.legendre_transform(sigma))
-        E_c.append(F_full[-1] - F_KS[-1])
-    
+        LT_full = E_full.legendre_transform([sigma, x])
+        LT_full_eps = E_full.legendre_transform([sigma, x], epsMY=eps)
+        LT_KS = E_KS.legendre_transform([sigma, x])
+        
+        #check_sigma_x(lam, sigma, x, LT_full['pot'][1])
+        #check_sigma_x(0, sigma, x, LT_KS['pot'][1])
+        
+        F_full.append(LT_full['F'])
+        F_full_eps.append(LT_full_eps['F'])
+        F_KS.append(LT_KS['F'])
+        E_xc.append(F_full[-1] - F_KS[-1])
+        
+        # solve KS and full system
+        sol_full = E_full.solve(LT_full['pot'])
+        sol_KS = E_KS.solve(LT_KS['pot'])
+        
+        test_F_full.append(sol_full['gs_energy'] - np.dot(LT_full['pot'], [sigma, x]))
+        test_F_KS.append(sol_KS['gs_energy'] - np.dot(LT_KS['pot'], [sigma, x]))
+        # get correlation parts
+        W_0.append(lam*CouplingRabi.expval(sol_KS['gs_vector'], check_real=True))
+        W_c.append(lam*CouplingRabi.expval(sol_full['gs_vector'], check_real=True) - W_0[-1])
+        T_c.append(H0_KS.expval(sol_full['gs_vector'], check_real=True) - H0_KS.expval(sol_KS['gs_vector'], check_real=True))
+        test_E_xc.append(W_0[-1] + (W_c[-1] + T_c[-1])) 
+
     fig1, ax1 = plt.subplots(ncols=1, nrows=1, figsize=(16,9))
-    ax1.set_title(r'Universal functional - Non-interacting system ($\lambda=0$)')
+    ax1.set_title(r'Universal functional - Non-interacting system ($\lambda=0$)',fontsize=40)
     ax1.set_xlabel(r'$\sigma$')
     ax1.set_ylabel(r'$\mathcal{F}_{LL}(\sigma,0)$')
 
     fig2, ax2 = plt.subplots(ncols=1, nrows=1, figsize=(16,9))
-    ax2.set_title(r'Universal functional - Interacting system ($\lambda=1$)')
+    ax2.set_title(r'Universal functional - Interacting system ($\lambda=1$)',fontsize=40)
     ax2.set_xlabel(r'$\sigma$')
     ax2.set_ylabel(r'$\mathcal{F}_{LL}(\sigma,0)$')
 
@@ -69,10 +109,11 @@ def plot_functionals():
     ax1.plot(sigma_space, F_KS, '--', label=r'Numerical')
     
     # Plot fully interacting
-    ax2.plot(sigma_space, 0.5-t*np.sqrt(1-np.square(sigma_space)) - np.abs(sigma_space)*(lam**2)/2, label=r'Analytic Approx')
-    ax2.plot(sigma_space, F_full, label=r'Numerical')
-    ax2.plot(sigma_space, F_full_eps, label=r'$F_\varepsilon^{\lambda=1}(\sigma,0)$' + fr', $\varepsilon={eps}$')
+    ax2.plot(sigma_space, 0.5-t*np.sqrt(1-np.square(sigma_space)) - (1-sigma_space**2)*(lam**2)/4, label=r'Analytic Approx')
+    ax2.plot(sigma_space, F_full,'.', label=r'Numerical')
+    ax2.plot(sigma_space, F_full_eps, '.', label=r'$F_\varepsilon^{\lambda=1}(\sigma,0)$' + fr', $\varepsilon={eps}$')
     #ax.plot(sigma_space, np.array(E_c), 'r', label=r'$E_c(\sigma,0)$')
+    ax2.plot(sigma_space, 0.5 - t*np.sqrt(1-sigma_space**2) - 1*lam**2/4, label=r'Lower bound')
 
     ax1.legend()
     ax2.legend()
